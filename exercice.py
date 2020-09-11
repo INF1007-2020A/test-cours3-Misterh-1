dbbec9195a277fd5c2a471a4a085b50c65585a89
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-11 00:24:04 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
-    # TODO completer la fonction
+        new_letter = []
+    new_name = ''
+    for j in range(len(nom)):
+        letter_num = ord(nom[j])
+        if j == 0 :
+            if letter_num >= 97 and letter_num <= 122 :
+                letter_num = letter_num - 32
+        else :
+            if letter_num >= 65 and letter_num <= 90 :
+                letter_num = letter_num + 32
+        num_letter = chr(letter_num)
+        new_letter.append(num_letter)
+        new_name = new_name + new_letter[j]
+        #print(new_name)
+    nom = new_name
     return nom
 
 
